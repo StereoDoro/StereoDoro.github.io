1ab0ff9f555d6b1f088224dfca9f82c3d4abd6bd
StereoDoro <127454088+StereoDoro@users.noreply.github.com>
2024-05-02 16:14:02 -0400

    Add files via upload
    NYSA Presents Season 9 ep. 13 05/05/2024 - Part 1

---
+++ nysa_20240505_s9_13/!! SPM HTML5 to Stereopix JSON.py
@@ -0,0 +1,22 @@
+"""
+For creating a list of images formated for Stereopix JSON
+from a Stereo Photomaker HTML5 file named
+    "index_m.html".
+
+The output list is in the file
+    "JSON format image list.txt"
+"""
+
+spmfile = open("index_m.html", 'r').read().splitlines()
+outlist = []
+for line in spmfile:
+    line = line.split("?")
+    try:
+        outlist.append('{ "url": "%s" }' % (line[1].split(('"'))[0]))
+    except IndexError:
+        pass
+
+open("JSON format image list.txt", 'w').write(",\n".join(outlist))
+
+
+wait = input("Press Enter to continue.")
